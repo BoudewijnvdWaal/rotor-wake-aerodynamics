4d10d1fdc0e5f4a1d698de91060cb0473c8538c4
tgod <tgod@student.tudelft.nl>
2026-03-16 15:25:23 +0100

    code werkt

--- BEM_model.py
+++ BEM_model.py
@@ -13,12 +13,13 @@ import pandas as pd
 # BLOCK 0.1 : Rotor specs
 Radius = 50 # m
 blades = 3 # number of blades
-blade_start = 0.2 * Radius # m, distance from center where blades start
+RootLocation_R = 0.2 # m, distance from center where blades start
+TipLocation_R = 1.0 # m, distance from center where blades end
 blade_pitch = -2 # degrees, pitch angle at the root of the blade
 
 # BLOCK 0.2 : Section streamtubes
 delta_r_R = 0.01 # non-dimensioned width of blade element, in fraction of rotor radius
-r_R = np.arange(blade_start/Radius, 1 + delta_r_R, delta_r_R) # non-dimensioned radial position of blade element, in fraction of rotor radius
+r_R = np.arange(RootLocation_R, TipLocation_R + delta_r_R, delta_r_R) # non-dimensioned radial position of blade element, in fraction of rotor radius
 chord_distribution = 3*(1-r_R) + 1 # m, chord distribution along the blade, linearly decreasing from 4 m at the root to 1 m at the tip
 twist_distribution = 14*(1-r_R) # degrees, twist distribution along the blade,
 
@@ -42,12 +43,40 @@ airfoil = 'polar DU95W180.xlsx'
 data=pd.read_excel(airfoil, header=0,
                     names = ["alpha", "cl", "cd", "cm"]).dropna()
 data = data.drop(data.index[0]) # drop first row with header info
-polar_alpha = data['alpha'][:]
-polar_cl = data['cl'][:]
-polar_cd = data['cd'][:]
+polar_alpha = data['alpha'][:].astype(float)
+polar_cl = data['cl'][:].astype(float)
+polar_cd = data['cd'][:].astype(float)
 
 
 # --------- Module 2 : Normal and Tangential Forces ---------
+
+# BLOCK 2.3 : Calculate thrust coefficient as a function of induction factor, with option to include Glauert's correction for heavily loaded rotors, and calculate axial induction as a function of thrust coefficient, including Glauert's correction
+def CTfunction(a, glauert_correction=False):
+    """
+    This function calculates the thrust coefficient as a function of induction factor 'a'
+    'glauert' defines if the Glauert correction for heavily loaded rotors should be used; default value is false
+    """
+    CT = np.zeros(np.shape(a))
+    CT = 4*a*(1-a)  
+    if glauert_correction:
+        CT1=1.816
+        a1=1-np.sqrt(CT1)/2
+        CT[a>a1] = CT1-4*(np.sqrt(CT1)-1)*(1-a[a>a1])
+
+    return CT
+
+# BLOCK 2.4 : Calculate axial induction as a function of thrust coefficient, including option to include Glauert's correction for heavily loaded rotors
+def ainduction(CT):
+    """
+    This function calculates the induction factor 'a' as a function of thrust coefficient CT 
+    including Glauert's correction
+    """
+    a = np.zeros(np.shape(CT))
+    CT1=1.816
+    CT2=2*np.sqrt(CT1)-CT1
+    a[CT>=CT2] = 1 + (CT[CT>=CT2]-CT1)/(4*(np.sqrt(CT1)-1))
+    a[CT<CT2] = 0.5-0.5*np.sqrt(1-CT[CT<CT2])
+    return a
 
 # Block 2.1 : Prandtl's tip and root loss correction
 def PrandtlTipRootCorrection(r_R, rootradius_R, tipradius_R, TSR, NBlades, axial_induction):
@@ -80,34 +109,6 @@ def loadBladeElement(vnorm, vtan, r_R, chord, twist, polar_alpha, polar_cl, pola
     gamma = 0.5*np.sqrt(vmag2)*cl*chord
     return fnorm , ftan, gamma
 
-# BLOCK 2.3 : Calculate thrust coefficient as a function of induction factor, with option to include Glauert's correction for heavily loaded rotors, and calculate axial induction as a function of thrust coefficient, including Glauert's correction
-def CTfunction(a, glauert_correction=False):
-    """
-    This function calculates the thrust coefficient as a function of induction factor 'a'
-    'glauert' defines if the Glauert correction for heavily loaded rotors should be used; default value is false
-    """
-    CT = np.zeros(np.shape(a))
-    CT = 4*a*(1-a)  
-    if glauert_correction:
-        CT1=1.816
-        a1=1-np.sqrt(CT1)/2
-        CT[a>a1] = CT1-4*(np.sqrt(CT1)-1)*(1-a[a>a1])
-
-    return CT
-
-# BLOCK 2.4 : Calculate axial induction as a function of thrust coefficient, including option to include Glauert's correction for heavily loaded rotors
-def ainduction(CT):
-    """
-    This function calculates the induction factor 'a' as a function of thrust coefficient CT 
-    including Glauert's correction
-    """
-    a = np.zeros(np.shape(CT))
-    CT1=1.816
-    CT2=2*np.sqrt(CT1)-CT1
-    a[CT>=CT2] = 1 + (CT[CT>=CT2]-CT1)/(4*(np.sqrt(CT1)-1))
-    a[CT<CT2] = 0.5-0.5*np.sqrt(1-CT[CT<CT2])
-    return a
-
 
 # --------- Module 3 : Streamtube Model ---------
 
@@ -128,11 +129,13 @@ def solveStreamtube(Uinf, r1_R, r2_R, rootradius_R, tipradius_R , Omega, Radius,
     # initiatlize variables
     a = 0.0 # axial induction
     aline = 0.0 # tangential induction factor
+    anew = 1.0 # new estimate of axial induction, for iteration process
     
-    Niterations = 100
-    Erroriterations =0.00001 # error limit for iteration process, in absolute value of induction
+    iteration = 0
+    Erroriterations = 0.00001 # error limit for iteration process, in absolute value of induction
     
-    for i in range(Niterations):
+    while np.abs(a-anew) > Erroriterations:
+        iteration += 1
         # calculate velocity and loads at blade element
         Urotor = Uinf*(1-a) # axial velocity at rotor
         Utan = (1+aline)*Omega*r_R*Radius # tangential velocity at rotor
@@ -157,22 +160,32 @@ def solveStreamtube(Uinf, r1_R, r2_R, rootradius_R, tipradius_R , Omega, Radius,
         # calculate aximuthal induction
         aline = ftan*NBlades/(2*np.pi*Uinf*(1-a)*Omega*2*(r_R*Radius)**2)
         aline =aline/Prandtl # correct estimate of azimuthal induction with Prandtl's correction
-        
-        # test convergence of solution, by checking convergence of axial induction
-        if (np.abs(a-anew) < Erroriterations): 
-            break
 
     return [a , aline, r_R, fnorm , ftan, gamma]
 
-# --------- Module 4 : Calculate Loads and Power ---------
+# --------- Module 4 : BEM executor ---------
+
+# BLOCK 4.1 : Execute BEM solver for a given set of input parameters, and return the distribution of axial induction, tangential induction, loads and circulation along the blade
+def executeBEM(Uinf, TSR, RootLocation_R, TipLocation_R , Omega, Radius, NBlades, chord_distribution, twist_distribution, polar_alpha, polar_cl, polar_cd):
+    results = np.zeros([len(r_R)-1, 6])
+
+    for i in range(len(r_R)-1):
+        chord = np.interp((r_R[i]+r_R[i+1])/2, r_R, chord_distribution)
+        twist = np.interp((r_R[i]+r_R[i+1])/2, r_R, twist_distribution)
+        results[i,:] = solveStreamtube(Uinf, r_R[i], r_R[i+1], RootLocation_R, TipLocation_R , Omega, Radius, NBlades, chord, twist, polar_alpha, polar_cl, polar_cd)
+
+    areas = (r_R[1:]**2-r_R[:-1]**2)*np.pi*Radius**2
+    dr = (r_R[1:]-r_R[:-1])*Radius
+    CT = np.sum(dr*results[:,3]*blades/(0.5*U0**2*np.pi*Radius**2))
+    CP = np.sum(dr*results[:,4]*results[:,2]*blades*Radius*Omega/(0.5*U0**3*np.pi*Radius**2))
+
+    print("CT is ", CT)
+    print("CP is ", CP)
 
 
-
-
-
-# --------- Module 4 : Visualiser ---------
+# --------- Module 5 : Visualiser ---------
 """
-# BLOCK 4.1 :plot Prandtl tip, root and combined correction for a number of blades and induction 'a', over the non-dimensioned radius
+# BLOCK 5.1 :plot Prandtl tip, root and combined correction for a number of blades and induction 'a', over the non-dimensioned radius
 Prandtl, Prandtltip, Prandtlroot = PrandtlTipRootCorrection(r_R, blade_start/Radius, 1, TSR[0], blades, a)
 
 fig1 = plt.figure(figsize=(12, 6))
@@ -183,7 +196,8 @@ plt.xlabel('r/R')
 plt.legend()
 """
 
-# BLOCK 4.2 : plot airfoil polars as a function of angle of attack
+"""
+# BLOCK 5.2 : plot airfoil polars as a function of angle of attack
 fig, axs = plt.subplots(1, 2, figsize=(12, 6))
 axs[0].plot(polar_alpha, polar_cl)
 axs[0].set_xlim([-30,30])
@@ -195,38 +209,12 @@ axs[1].set_xlim([0,.1])
 axs[1].set_xlabel(r'$C_d$')
 axs[1].grid()
 plt.show()
+"""
 
-# --------- Module 5 : BEM Executor ---------
+def main():
+    for i in range(len(TSR)):
+        print("Executing BEM for TSR = ", TSR[i])
+        executeBEM(U0, TSR[i], RootLocation_R, TipLocation_R , Omega[i], Radius, blades, chord_distribution, twist_distribution, polar_alpha, polar_cl, polar_cd)
 
-# BLOCK 5.1 : Execute BEM solver for a given set of input parameters, and return the distribution of axial induction, tangential induction, loads and circulation along the blade
-def executeBEM(Uinf, TSR, rootradius_R, tipradius_R , Omega, Radius, NBlades, chord_distribution, twist_distribution, polar_alpha, polar_cl, polar_cd):
-    a_distribution = np.zeros(np.shape(r_R))
-    aline_distribution = np.zeros(np.shape(r_R))
-    fnorm_distribution = np.zeros(np.shape(r_R))
-    ftan_distribution = np.zeros(np.shape(r_R))
-    gamma_distribution = np.zeros(np.shape(r_R))
-
-    for i in range(len(r_R)):
-        r1_R = r_R[i]-delta_r_R/2
-        r2_R = r_R[i]+delta_r_R/2
-        chord = chord_distribution[i]
-        twist = twist_distribution[i]
-        a_distribution[i], aline_distribution[i], r_R[i], fnorm_distribution[i], ftan_distribution[i], gamma_distribution[i] = solveStreamtube(Uinf, r1_R, r2_R, rootradius_R, tipradius_R , Omega, Radius, NBlades, chord, twist, polar_alpha, polar_cl, polar_cd)
-
-    return a_distribution, aline_distribution, fnorm_distribution, ftan_distribution, gamma_distribution
-
-results = np.zeros([len(r_R)-1, 6, len(TSR)])
-
-for i in range(len(TSR)):
-    chord = np.interp((r_R[i]+r_R[i+1])/2, r_R, chord_distribution)
-    twist = np.interp((r_R[i]+r_R[i+1])/2, r_R, twist_distribution)
-
-    results[:,:,i] = executeBEM(U0, TSR[i], blade_start/Radius, 1 , Omega[i], Radius, blades, chord_distribution, twist_distribution, polar_alpha, polar_cl, polar_cd)
-
-areas = (r_R[1:]**2-r_R[:-1]**2)*np.pi*Radius**2
-dr = (r_R[1:]-r_R[:-1])*Radius
-CT = np.sum(dr*results[:,3]*blades/(0.5*U0**2*np.pi*Radius**2))
-CP = np.sum(dr*results[:,4]*results[:,2]*blades*Radius*Omega/(0.5*U0**3*np.pi*Radius**2))
-
-print("CT is ", CT)
-print("CP is ", CP)
+if __name__ == "__main__":
+    main()
